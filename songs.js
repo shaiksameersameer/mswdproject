968990af51cc4234354b768cf8a420a1a9591dbc
shaiksameersameer <152957348+shaiksameersameer@users.noreply.github.com>
2024-04-01 23:45:47 +0530

    .vscode

--- songs.js
+++ songs.js
@@ -1,64 +1,25 @@
-const router = require("express").Router();
-const { User } = require("../models/user");
-const { Song, validate } = require("../models/song");
-const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
-const validateObjectId = require("../middleware/validateObjectId");
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-// Create song
-router.post("/", admin, async (req, res) => {
-	const { error } = validate(req.body);
-	if (error) res.status(400).send({ message: error.details[0].message });
-
-	const song = await Song(req.body).save();
-	res.status(201).send({ data: song, message: "Song created successfully" });
+const songSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	artist: { type: String, required: true },
+	song: { type: String, required: true },
+	img: { type: String, required: true },
+	duration: { type: String, required: true },
 });
 
-// Get all songs
-router.get("/", async (req, res) => {
-	const songs = await Song.find();
-	res.status(200).send({ data: songs });
-});
-
-// Update song
-router.put("/:id", [validateObjectId, admin], async (req, res) => {
-	const song = await Song.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
+const validate = (song) => {
+	const schema = Joi.object({
+		name: Joi.string().required(),
+		artist: Joi.string().required(),
+		song: Joi.string().required(),
+		img: Joi.string().required(),
+		duration: Joi.number().required(),
 	});
-	res.send({ data: song, message: "Updated song successfully" });
-});
+	return schema.validate(song);
+};
 
-// Delete song by ID
-router.delete("/:id", [validateObjectId, admin], async (req, res) => {
-	await Song.findByIdAndDelete(req.params.id);
-	res.status(200).send({ message: "Song deleted sucessfully" });
-});
+const Song = mongoose.model("song", songSchema);
 
-// Like song
-router.put("/like/:id", [validateObjectId, auth], async (req, res) => {
-	let resMessage = "";
-	const song = await Song.findById(req.params.id);
-	if (!song) return res.status(400).send({ message: "song does not exist" });
-
-	const user = await User.findById(req.user._id);
-	const index = user.likedSongs.indexOf(song._id);
-	if (index === -1) {
-		user.likedSongs.push(song._id);
-		resMessage = "Added to your liked songs";
-	} else {
-		user.likedSongs.splice(index, 1);
-		resMessage = "Removed from your liked songs";
-	}
-
-	await user.save();
-	res.status(200).send({ message: resMessage });
-});
-
-// Get liked songs
-router.get("/like", auth, async (req, res) => {
-	const user = await User.findById(req.user._id);
-	const songs = await Song.find({ _id: user.likedSongs });
-	res.status(200).send({ data: songs });
-});
-
-module.exports = router;
+module.exports = { Song, validate };
